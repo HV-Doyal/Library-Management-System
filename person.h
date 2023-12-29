e77eec4ae2cbc976561062455b9101da27a93426
Harshvardhan Doyal <hd499@live.mdx.sc.uk>
2023-12-29 21:05:26 +0400

    Created class person with info required

--- person.h
+++ person.h
@@ -7,9 +7,44 @@
     Updated: 29/12/2023
 */
 
+#include <string>
+
 class Person
 {
+    private:
+        std::string name;
+        std::string address;
+        std::string email;
+    
+    public:
+        void setName(std::string nm)
+        {
+            name = nm;
+        }
+        std::string getName()
+        {
+            return name;
+        }
 
+        void setAddress(std::string add)
+        {
+            address = add;
+        }
+
+        std::string getAddress()
+        {
+            return address;
+        }
+
+        void setEmail(std::string eml)
+        {
+            email = eml;
+        }
+
+        std::string getEmail()
+        {
+            return email;
+        }
 };
 
 #endif
