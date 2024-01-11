206be899c93deaad529ac5508e3e1262616d3247
Harshvardhan Doyal <hd499@live.mdx.sc.uk>
2024-01-11 14:56:29 +0400

    Created class book

--- book.cpp
+++ book.cpp
@@ -4,3 +4,75 @@
     Created: 09/01/2023
     Updated: 09/01/2023
 */
+#include "book.h"
+#include "date.h"
+
+Book::Book(int bookID, std::string bookName, std::string authorFirstName, 
+           std::string authorLastName, std::string bookType)
+           : bookID(bookID), bookName(bookName), 
+           authorFirstName(authorFirstName), authorLastName(authorLastName), 
+           bookType(bookType), dueDate(), borrower()
+{
+}
+
+int Book::getBookID() 
+{
+    return bookID;
+}
+
+std::string Book::getBookName() 
+{
+    return bookName;
+}
+
+std::string Book::getAuthorFirstName() 
+{
+    return authorFirstName;
+}
+
+std::string Book::getAuthorLastName() 
+{
+    return authorLastName;
+}
+
+Date Book::getDueDate() 
+{
+    return *dueDate;
+}
+
+void Book::setDueDate(Date dueDate)
+{
+    this->dueDate = new Date(dueDate);
+}
+
+void Book::returnBook()
+{
+    if (borrower != nullptr)
+    {
+        std::cout << "Returning book " << bookID << " from " 
+                  << borrower->getName() << std::endl;
+        borrower = nullptr;
+        delete dueDate;
+        dueDate = nullptr;
+    }
+    else
+    {
+        std::cout << "Book " << bookID << " is not currently borrowed." 
+                  << std::endl;
+    }
+}
+
+void Book::borrowBook(Member *borrower, Date dueDate)
+{
+    if (this->borrower == nullptr)
+    {
+        this->borrower = borrower;
+        this->dueDate = new Date(dueDate);
+        std::cout << borrower->getName() << " borrowed book " << bookID 
+                  << " with due date " << dueDate.toString() << std::endl;
+    }
+    else
+    {
+        std::cout << "Book " << bookID << " is already borrowed." << std::endl;
+    }
+}
--- book.h
+++ book.h
@@ -7,12 +7,39 @@
     Updated: 29/12/2023
 */
 
-#include "librarian.h"
+#ifndef _BOOK_H_
+#define _BOOK_H_
+
+#include <string>
 #include "member.h"
+#include "librarian.h"
+
+class Date;  // Forward declaration of the Date class
 
 class Book
 {
+private:
+    int bookID;
+    std::string bookName;
+    std::string authorFirstName;
+    std::string authorLastName;
+    std::string bookType;
+    Date *dueDate;
+    Member *borrower;
 
+public:
+    Book(int bookID, std::string bookName, std::string authorFirstName, 
+         std::string authorLastName, std::string bookType);
+
+    int getBookID();
+    std::string getBookName();
+    std::string getAuthorFirstName();
+    std::string getAuthorLastName();
+    Date getDueDate();
+
+    void setDueDate(Date dueDate);
+    void returnBook();
+    void borrowBook(Member *borrower, Date dueDate);
 };
 
 #endif
